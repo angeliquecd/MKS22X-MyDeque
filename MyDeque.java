737d12e4178cd8b9a78dbc213e8110422795729e
angeliquecd <acharles-davis00@stuy.edu>
2019-03-24 16:11:07 -0400

    pulling

--- MyDeque.java
+++ MyDeque.java
@@ -2,6 +2,7 @@
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
+
 @SuppressWarnings("unchecked")
   public MyDeque(){
 	   data = (E[]) new Object[10];
@@ -9,46 +10,56 @@ public class MyDeque<E>{
 	     start = 0;
 	      end = 0;
  }//suppress warning
+
  @SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity){
 	   data = (E[]) new Object[initialCapacity];
-	    size = initialCapacity;
+	    size = 0;
 	     start=0;
 	      end=0; }
+
   public int size(){
 	   return size;}
+
   public String toString(){
     String value ="";
     int total =start;
     int inc =0;
     int level=0;
-    while (inc<=size){
-      value+=data[total+level%size]+" ";
+    while (inc<size){
+      value+=data[(total+level)%size]+" ";
       inc++;
       level++;
     }
     return value;
   }
+
   public void addFirst(E element){
+    if(size!=1){
 	   start=size;
      data[size]=element;
-     size++;
+     size++;}
 	}
+
   public void addLast(E element){
 	data[size]=element;
 	end = size;
-	size++;}
+	size++;
+}
   public E removeFirst(){
+    data[start]=null;
     if (start+1!=end && start+1<size) start = start+1;
 	size--;
   return data[start];}
+
   public E removeLast(){
 	int endy =end;
-	end = data[size-2];
+	end = size;
 	size--;
 	return data[endy];}//pop
+
   public E getFirst(){
-	return start; }//do not remove the element
+	return data[start]; }//do not remove the element
   public E getLast(){
-	return end; }
+	return data[end]; }
 }
